1057b203c0b7e92dcb91fb5cd9d210fca8b20336
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-12 23:46:32 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,22 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+
+    def change
+      create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.float :price
+
+        t.integer :costume_inventory
+        t.integer :num_of_employees
+        t.boolean :still_in_business
+        t.time :start_time
+        t.time :end_time
+        t.boolean :family_friendly
+        t.datetime :opening_date
+        t.datetime :closing_date
+        t.string :description
+      end
+    end
+
+end
